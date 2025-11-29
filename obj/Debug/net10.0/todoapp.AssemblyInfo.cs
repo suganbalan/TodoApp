395da0a09851a7b6c20281afa860b54dfa6642d9
suganbalan <suganbalan06042000@gmail.com>
2025-11-29 20:34:05 +0530

    Initial commit

--- obj/Debug/net10.0/todoapp.AssemblyInfo.cs
+++ obj/Debug/net10.0/todoapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todoapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9790301e49a142d9c26d456ec9719d399b401a")]
 [assembly: System.Reflection.AssemblyProductAttribute("todoapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todoapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
